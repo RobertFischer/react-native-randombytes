7d79acd2a2397c1558ee9f2fdbc8ec68f292c701
Robert Fischer <robert.fischer@k4connect.com>
2020-07-14 18:01:39 -0400

    Implement semantic-release

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	branches: ["master"],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		"@semantic-release/git",
+		"@semantic-release/github",
+	],
+	assets: ["CHANGELOG.md", "package.json", "yarn.lock"],
+	preset: "conventionalcommits",
+	linkCompare: true,
+	linkReferences: true,
+	assignees: ["RobertFischer"],
+};
